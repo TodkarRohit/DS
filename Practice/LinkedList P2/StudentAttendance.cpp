d249c683e878a98555e2d61826cd6af0e3467244
whoiam27 <rohittodkar92@gmail.com>
2026-07-31 14:38:45 +0530

    ADDed node & main fun

--- Practice/LinkedList P2/StudentAttendance.cpp
+++ Practice/LinkedList P2/StudentAttendance.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 using namespace std;
 
+class Node
+{
+  public:
+    
+    int data;
+    Node *next;
+    
+    Node(int value)
+    {
+      data = value;
+      next = NULL;
+    }
+}; //class Node
+
 class LinkedList
 {
     
 
 };
+
+int main()
+{
+  return 0; 
+}
