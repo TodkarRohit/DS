a4f588891b0ef83db9e8534b34a7617aa2a02215
ThoratSoham <djsoham02@gmail.com>
2026-07-31 09:07:48 +0530

    Creating LinkedList Class

--- Practice/LinkedList P2/StudentAttendance.cpp
+++ Practice/LinkedList P2/StudentAttendance.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 using namespace std;
 
+
 class LinkedList
 {
-    
+    public:
+        Node *head;
+        LinkedList()
+        {
+            head = NULL;
+        }
+
+        void add()
+        {
+            Node *n1 = new Node(value)
+        }
+
+
 
 };
